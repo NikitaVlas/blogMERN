7386ec9d5b2ccca971653038737edd83728b5aa3
Nikita Vlasov <84715117+NikitaVlas@users.noreply.github.com>
2023-11-02 15:41:24 +0100

    login

---
+++ server/controllers/auth.js
@@ -0,0 +1,77 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+//Register
+export const register = async (req, res) => {
+    try {
+        const {username, password} = req.body
+
+        const isUsed = await User.findOne({username})
+        if(isUsed) {
+            return res.json({
+                message: 'This username already in use'
+            })
+        }
+
+        const salt = bcrypt.genSaltSync(10) //сложность хешироного пароля
+        const hash = bcrypt.hashSync(password, salt)
+
+        const newUser = new User ({
+            username,
+            password: hash
+        })
+
+        await newUser.save()
+
+        res.json({
+            newUser, message: 'Registration completed successfully'
+        })
+
+    } catch (error){
+        res.json({message: 'Error creating user'})
+    }
+}
+
+// Login
+export const login = async (req, res) => {
+    try {
+        const{username, password} = req.body
+        const user = await User.findOne({username})
+        if(!user) {
+            return res.json({
+                message: "This user doesn't exist"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect) {
+            return res.json({
+                message: "Incorrect password"
+            })
+        }
+
+        // шифруем токен
+        const token = jwt.sign({
+            id: user._id,
+        },
+            process.env.JWT_SECRET,
+            {expiresIn: "30d"}
+        )
+
+        res.json({
+            token, user, message: "You are logged in"
+        })
+
+    } catch (error){
+        res.json({message: 'Authorisation Error '})
+    }
+}
+//Get me
+export const getMe = async (req, res) => {
+    try {
+
+    } catch (error){
+
+    }
+}
